0c5bd6a4f3de22dc03e1bcea5ab81b6b3fd63abd
gemmiedodger <gemmadruce@live.co.uk>
2020-10-04 14:31:38 +0100

    adding further experimentation and restaurant, menu + item

--- models.js
+++ models.js
@@ -7,9 +7,20 @@ class Restaurant {
         restaurant.id = data.id
         restaurant.name = data.name
         restaurant.image = data.image
+        restaurant.menus = []
 
+        
         if (restaurant.id) {
-            return Promise.resolve(restaurant)
+            return new Promise((resolve, reject) => {
+                db.all('SELECT * FROM menus WHERE restaurant_id=?;', [restaurant.id], (err,rows) => {
+                    const arrayOfPromises = rows.map(row => new Menu(row))
+                    Promise.all(arrayOfPromises).then((menus) => {
+                        restaurant.menus = menus
+                        console.log(restaurant.menus)
+                        resolve(restaurant)
+                    }).catch(err => reject(err)) //do all promises and then, if error catch
+                })
+            })
         } else {
             console.log('outside the db callabck', restaurant,this)
 
@@ -26,6 +37,11 @@ class Restaurant {
 
         }
     }
+    async addMenu(data) {
+        const menu = await new Menu({title: data.title, restaurant_id: this.id})
+        this.menus.push(menu)
+    }
+
 }
 
 class Menu {
@@ -34,8 +50,10 @@ class Menu {
         menu.id = data.id
         menu.title = data.title
         menu.restaurant_id = data.restaurant_id
+        menu.items = []
 
         if (menu.id) {
+             
             return Promise.resolve(menu) 
         } else {
             return new Promise((resolve, reject) => {
@@ -43,12 +61,15 @@ class Menu {
                 function (err) {
                     if (err) return reject(err)
                     menu.id = this.lastID
-                    console.log("insde of the callback menu", menu, this)
+                    console.log("inside of the callback menu", menu, this)
                     return resolve(menu)
                 })
             })
         }
-
+    }
+    async addItem(data) {
+        const item = await new Item({name: data.name, price: data.price, menu_id: this.id})
+            this.items.push(item)
     }
 }
 
--- models.test.js
+++ models.test.js
@@ -23,47 +23,115 @@ describe('Restaurant', () => {
             done()
             })
     })
-
-    test('check menu is added to database', async () => {
-        const menu = await new Menu({title:'Dinner',restaurant_id:1})
-        expect(menu.id).toBe(1)
-      
-    })
-
-    test('create a menu for the data row', (done) => {
-        db.get('SELECT * FROM menus WHERE id = 1;', async (err, row) => {
-            
-            expect(row.title).toBe("Dinner")
-            const menu = await new Menu(row)
-            expect(menu.id).toBe(1)
-            expect(menu.title).toBe('Dinner')
+    test('a restaurant should have menus', async (done) => {
+        const restaurant = await new Restaurant({name: "Rice-and-Pot", images: "url"})
+        expect(restaurant.menus.length).toBe(0)
+        // const menu = await new Menu({title: "Dessert", restaurant_id: restaurant.id})
+        await restaurant.addMenu({title: "Dessert"})
+        expect(restaurant.menus[0] instanceof Menu).toBeTruthy()
+        expect(restaurant.menus[0].id).toBeTruthy()
+        await restaurant.addMenu({title: "Child's Menu"})
+        await restaurant.addMenu({title: "Wine Menu"})
+        
+        db.get('SELECT * FROM restaurants WHERE id=?', [restaurant.id], async (err, row) => {
+            console.log(row)
+            const ricepot = await new Restaurant(row)
+            expect(ricepot.id).toBe(restaurant.id)
+            expect(ricepot.menus.length).toBe(3)
+            expect(ricepot.menus[0] instanceof Menu).toBeTruthy()
             done()
-            })
+        })
     })
+        //MENUS
+        // test('a menu should have items', async(done) => {
+        //     //const restaurant = await new Restaurant({name:'Happy Eaters', images: "url"})
+        //     const menu = await new Menu({title:"Dinner", restaurant_id:1})
+        //     expect(menu.items.length).toBe(0)
+        //     await menu.addMenu({name:"Wine"})
+        //     expect(menu.items[0] instanceof Menu).toBeTruthy
+        //     expect(menu.menus[0].id)
+        // })   
+})
 
+
+describe('Menus', () => {
+    // beforeAll((done) => {
+    //     db.exec('CREATE TABLE restaurants(id INTEGER PRIMARY KEY, name TEXT, image TEXT); CREATE TABLE menus(id INTEGER PRIMARY KEY, title TEXT, restaurant_id INTEGER); CREATE TABLE items(id INTEGER PRIMARY KEY, name TEXT, price TEXT, menu_id); ', done)
+    // })
+        
+        test('check menu is added to database', async () => {
+            const menu = await new Menu({title:'Dinner',restaurant_id:1})
+            expect(menu.id).toBe(4)
+          
+        })
+    
+        test('create a menu for the data row', (done) => {
+            db.get('SELECT * FROM menus WHERE id = 4;', async (err, row) => {
+                
+                expect(row.title).toBe("Dinner")
+                const menu = await new Menu(row)
+                expect(menu.id).toBe(4)
+                expect(menu.title).toBe('Dinner')
+                done()
+                })
+        })
+        //check you can assign a menu to a restaurant
+ 
+        test('check items are assigned to menus + restaurant', async () => {
+            const restaurant = await new Restaurant({name: 'An apple a day', image:"tastyfruit.jpg"})
+            const menu = await new Menu({title:"Breakfast", restaurant_id: restaurant.id})
+            const item = await new Item({name: "Full English Breakfast", price: "7.00", menu_id: menu.id})
+            const items = menu.addItem(item)
+            const menus = restaurant.addMenu(menu)
+            expect(menu.title).toBe('Breakfast')
+            expect(menu.id).toBe(5)
+            console.log('this should be array breakfast')
+            console.log(items)
+            console.log("this should be list of restaurant")
+            console.log(restaurant)
+            console.log("this should be list of menu")
+            console.log(menus)
+
+
+            })
+
+        })
+     
+  
+
+describe('Item', () => {
     test('when a item is created add to a database', async () => {
         const item = await new Item({name:"Banana split", price: 3.00, menu_id:1})    
         // console.log(restaurant.id)
-        expect(item.id).toBe(1)
+        expect(item.id).toBe(3)
       
     })
    
 
     test('create a item for the data row', (done) => {
-        db.get('SELECT * FROM items WHERE id = 1;', async (err, row) => {
+        db.get('SELECT * FROM items WHERE id = 3;', async (err, row) => {
             expect(row.name).toBe("Banana split")
             const item = await new Item(row)
-            expect(item.id).toBe(1)
+            expect(item.id).toBe(3)
             expect(item.name).toBe("Banana split")
             done()
             })
     })
+    test('check multiple items assigned to multiple menus', async ()  => {
+        const restaurant = await new Restaurant({name: "Holy Pizza"})
+        const starter = await new Menu({title: "Starter"})
+        const main = await new Menu({title: "Main"})
+        const item1 = await new Item({name: "Pitta Bread and Houmous", price: 4.00, menu_id: 4})
+        const item2 = await new Item({name: "Olives", price: 3.00, menu_id: 4})
+        const item3 =  await new Item({name: "Hawaiian", price: 10.00, menu_id: 5})
+        const item4 = await new Item({name: "Pepperoni", price: 10.00, menu_id: 5})
+        const items = starter.addItem(item1, item2)
+        const items1 = main.addItem(item3, item4)
+        const menus = restaurant.addMenu(starter, main)
+        console.log(restaurant)
+    })    
     
-   
 })
-
-//wrap in a promise
-
 
 
 
